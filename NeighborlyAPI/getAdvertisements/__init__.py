6ad93fc2afa95ce2c4d0dca8715dc70ba046d609
burak <burakilhan@gmail.com>
2021-09-08 14:21:48 +0300

    Add files via upload

---
+++ NeighborlyAPI/getAdvertisements/__init__.py
@@ -0,0 +1,26 @@
+import azure.functions as func
+import pymongo
+import json
+from bson.json_util import dumps
+from bson.objectid import ObjectId
+import logging
+import os
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+
+    try:
+        url =  os.environ["mydbconnectionstring"]
+        client = pymongo.MongoClient(url)
+        database = client['azure']
+        collection = database['advertisements']
+
+
+        result = collection.find({})
+        result = dumps(result)
+
+        return func.HttpResponse(result, mimetype="application/json", charset='utf-8')
+    except:
+        print("could not connect to mongodb")
+        return func.HttpResponse("could not connect to mongodb",
+                                 status_code=400)
+
---
+++ NeighborlyAPI/getPost/__init__.py
@@ -0,0 +1,29 @@
+import azure.functions as func
+import pymongo
+import json
+from bson.json_util import dumps
+from bson.objectid import ObjectId
+import logging
+import os
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+
+    id = req.params.get('id')
+
+    if id:
+        try:
+            url =  os.environ["mydbconnectionstring"]
+            client = pymongo.MongoClient(url)
+            database = client['azure']
+            collection = database['posts']
+
+            query = {'_id': ObjectId(id)}
+            result = collection.find_one(query)
+            result = dumps(result)
+
+            return func.HttpResponse(result, mimetype="application/json", charset='utf-8')
+        except:
+            return func.HttpResponse("Database connection error.", status_code=500)
+
+    else:
+        return func.HttpResponse("Please pass an id parameter in the query string.", status_code=400)
